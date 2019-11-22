1a7adaca07b5003507f2bdaefc5228f552962bba
RPG3D <imxqliu@live.com>
2019-11-22 22:39:17 +0800

    1.,add test code

--- rpg3d/xmake.lua
+++ rpg3d/xmake.lua
@@ -1,33 +1,41 @@
 
 
 target("Lua")
-	set_kind("static")
-	add_files("../onelua.c")
-	--del_files("../lua.c")
+	set_kind("shared")
+	
+
 	set_languages("c11", "cxx14")
 	
+	add_files("../*.c")
+	del_files("../lua.c")
+	del_files("../onelua.c")
+
+
+	add_headerfiles("../*.h")
+
 	if (is_os("windows")) then
-		set_kind("shared")
 		add_defines("LUA_BUILD_AS_DLL=1")
 	end
 	
 	
 	if (is_os("android")) then
-		set_kind("shared")
 		add_defines("lua_getlocaledecpoint()=46")		
 		add_defines("ANDROID_STL=gnustl_shared")
 	end
 
-
+	if (is_os("ios")) then
+		set_kind("static")
+	end
 
 target("LuaMain")
 	set_kind("binary")
 	
-	add_includedirs("../")
-	add_files("../onelua.c")
+	add_files("../lua.c")
+
+	add_headerfiles("../*.h")
 	
-	--add_linkdirs("build/windows/x64/release")
-	--add_links("Lua")
+	add_linkdirs("build/windows/x64/release")
+	add_links("Lua")
 	
 	set_languages("c11", "cxx14")
 	
@@ -37,19 +45,3 @@ target("LuaMain")
 		set_enabled(false)
 	end
 
-
--- target("LuaC")
-	-- set_kind("binary")
-
-	-- add_includedirs("../")
-
-	-- add_files("../*.c")
-	-- del_files("src/lua.c")
-
-	-- set_languages("c11", "cxx14")
-
-	-- add_defines("LUA_BUILD_AS_DLL=1")
-
-	-- if (is_os("windows")) == false then
-		-- set_enabled(false)
-	-- end
